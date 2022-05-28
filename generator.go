9064c81e11863a60f704ba6f3f6c12b1f4a59272
fako1024 <mail+github@f-ko.eu>
2022-05-28 18:00:56 +0200

    Initial commit

---
+++ generator.go
@@ -0,0 +1,123 @@
+package randfiletree
+
+import (
+	"io/fs"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
+)
+
+const (
+	defaultSeed = 1
+)
+
+// Generator denotes a filetree generator
+type Generator struct {
+	basePath string
+	lastPath string
+
+	dirNameGen    FileNameGenerator
+	dirNameLenGen FileNameLenGenerator
+	dirModeGen    FileModeGenerator
+
+	nFilesInDirGen NumberGenerator
+	nDirsInDirGen  NumberGenerator
+
+	fileNameGen    FileNameGenerator
+	fileNameLenGen FileNameLenGenerator
+	fileModeGen    FileModeGenerator
+
+	dataGen      DataGenerator
+	pathDepthGen NumberGenerator
+
+	symlinkProbGen BooleanGenerator
+
+	rndSrc *rand.Rand
+}
+
+// New instantiates a new generator
+func New(basePath string) *Generator {
+	return &Generator{
+		basePath:       basePath,
+		dirNameGen:     StringGeneratorAlphabet(FileNameAlphabetBasic),
+		dirNameLenGen:  NumberGeneratorRandomFlat(1, 32),
+		dirModeGen:     FileModeGeneratorConstant(0755),
+		nFilesInDirGen: NumberGeneratorRandomFlat(1, 25),
+		nDirsInDirGen:  NumberGeneratorRandomFlat(0, 15),
+		fileNameGen:    StringGeneratorAlphabet(FileNameAlphabetBasic),
+		fileNameLenGen: NumberGeneratorRandomFlat(1, 64),
+		fileModeGen:    FileModeGeneratorConstant(0644),
+		dataGen:        DataGeneratorRandom(NumberGeneratorRandomFlat(0, 1024)),
+		pathDepthGen:   NumberGeneratorConstant(5),
+		symlinkProbGen: BooleanGeneratorProbabilityFlat(0.1),
+		rndSrc:         rand.New(rand.NewSource(defaultSeed)),
+	}
+}
+
+// Run generates a new tree (or adds to an existing one) according to the defined rules
+func (g *Generator) Run() error {
+	return g.writeDir(g.basePath, 0)
+}
+
+// Walk performs a recursive walk through the provided directory (wrapping filepath.Walk())
+func (g *Generator) Walk(fn filepath.WalkFunc) error {
+	return filepath.Walk(g.basePath, fn)
+}
+
+func (g *Generator) writeDir(path string, depth int) error {
+
+	// Check for depth abort criterion
+	depth++
+	if depth > g.pathDepthGen(g.rndSrc) {
+		return nil
+	}
+
+	// Create the directory
+	if err := os.MkdirAll(path, fs.FileMode(g.dirModeGen(g.rndSrc))); err != nil {
+		return err
+	}
+
+	// Create sub-directories, if any
+	for i := 0; i < g.nDirsInDirGen(g.rndSrc); i++ {
+		if err := g.writeDir(filepath.Join(path, g.dirNameGen(g.rndSrc, g.dirNameLenGen(g.rndSrc))), depth); err != nil {
+			return err
+		}
+	}
+
+	// Create files, if any
+	for i := 0; i < g.nFilesInDirGen(g.rndSrc); i++ {
+		if g.lastPath != "" && g.symlinkProbGen(g.rndSrc) {
+			if err := g.writeSymlinkInDir(path, g.lastPath); err != nil {
+				return err
+			}
+		} else {
+			if err := g.writeFileInDir(path); err != nil {
+				return err
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func (g *Generator) writeFileInDir(dir string) error {
+	path := filepath.Join(dir, g.fileNameGen(g.rndSrc, g.fileNameLenGen(g.rndSrc)))
+	mode := g.fileModeGen(g.rndSrc)
+	data, err := g.dataGen(g.rndSrc)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		g.lastPath = path
+	}()
+
+	return ioutil.WriteFile(path, data, fs.FileMode(mode))
+}
+
+func (g *Generator) writeSymlinkInDir(dir, target string) error {
+	path := filepath.Join(dir, g.fileNameGen(g.rndSrc, g.fileNameLenGen(g.rndSrc)))
+	return os.Symlink(target, path)
+}
---
+++ vars.go
@@ -0,0 +1,110 @@
+package randfiletree
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+const ()
+
+var (
+	FileNameAlphabetBasic = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890")
+	FileNameAlphabetLinux = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%^&*()-_+= ;.,")
+)
+
+// StringGenerator denotes a generic generator function for strings (e.g. for filenames)
+type StringGenerator func(r *rand.Rand, length int) string
+
+// FileNameGenerator is basically just a string generator
+type FileNameGenerator = StringGenerator
+
+// StringGeneratorAlphabet generates a string of requested length based on a provided alphabet
+func StringGeneratorAlphabet(alphabet []rune) StringGenerator {
+	return func(r *rand.Rand, length int) string {
+		b := make([]rune, length)
+		for i := range b {
+			b[i] = alphabet[r.Intn(len(alphabet))]
+		}
+		return string(b)
+	}
+}
+
+// NumberGenerator denotes a generic generator function for integers (e.g. for length of strings or data)
+type NumberGenerator func(r *rand.Rand) int
+
+// FileNameLenGenerator is basically just an integer number generator
+type FileNameLenGenerator = NumberGenerator
+
+// NumberGeneratorConstant generates a constant number
+func NumberGeneratorConstant(val int) NumberGenerator {
+	return func(r *rand.Rand) int {
+		return val
+	}
+}
+
+// NumberGeneratorRandomFlat generates a random number out of a range (equal probabilities)
+func NumberGeneratorRandomFlat(min, max int) NumberGenerator {
+	return func(r *rand.Rand) int {
+		return r.Intn(max-min) + min
+	}
+}
+
+// FileModeGenerator denotes a generic generator function for file modes (i.e. uint32)
+type FileModeGenerator func(r *rand.Rand) uint32
+
+// FileModeGeneratorConstant returns a fixed file mode
+func FileModeGeneratorConstant(mode uint32) FileModeGenerator {
+	return func(r *rand.Rand) uint32 {
+		return mode
+	}
+}
+
+// DataGenerator denotes a generic data generator
+type DataGenerator func(r *rand.Rand) ([]byte, error)
+
+// DataGeneratorFixed returns a fixed set of bytes
+func DataGeneratorFixed(data []byte) DataGenerator {
+	return func(r *rand.Rand) ([]byte, error) {
+		return data, nil
+	}
+}
+
+// DataGeneratorFixedString returns a fixed set of bytes based on a string
+func DataGeneratorFixedString(str string) DataGenerator {
+	return DataGeneratorFixed([]byte(str))
+}
+
+// DataGeneratorRandomFixedLen returns a random set of bytes of requested length
+func DataGeneratorRandomFixedLen(length int) DataGenerator {
+	return func(r *rand.Rand) ([]byte, error) {
+		data := make([]byte, length)
+		nRead, err := r.Read(data)
+		if err != nil || nRead != length {
+			return nil, fmt.Errorf("failed to generate random bytes: %s", err)
+		}
+		return data, nil
+	}
+}
+
+// DataGeneratorRandom returns a random set of bytes of randomized length
+func DataGeneratorRandom(lengthGen NumberGenerator) DataGenerator {
+	return func(r *rand.Rand) ([]byte, error) {
+		data := make([]byte, lengthGen(r))
+		nRead, err := r.Read(data)
+		if err != nil || nRead != len(data) {
+			return nil, fmt.Errorf("failed to generate random bytes: %s", err)
+		}
+		return data, nil
+	}
+}
+
+// BooleanGenerator generates a true / false value
+type BooleanGenerator func(r *rand.Rand) bool
+
+// BooleanGeneratorProbabilityFlat returns a random boolean with a given
+// probablity of being true
+func BooleanGeneratorProbabilityFlat(prob float64) BooleanGenerator {
+	return func(r *rand.Rand) bool {
+		return prob > r.Float64()
+	}
+}
